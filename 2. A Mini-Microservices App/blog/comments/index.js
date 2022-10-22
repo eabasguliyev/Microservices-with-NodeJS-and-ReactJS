7681834a1e45f33a6a3cb3b6720b5205b8d9c72f
elgun <elgun.abasquliyev2@gmail.com>
2022-10-22 08:27:41 +0400

    Create comments service

---
+++ 2. A Mini-Microservices App/blog/comments/index.js
@@ -0,0 +1,34 @@
+const { randomBytes } = require("crypto");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const COMMENTS = {};
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get("/posts/:id/comments", (req, res) => {
+  const { id } = req.params;
+
+  res.json(COMMENTS[id]);
+});
+
+app.post("/posts/:id/comments", (req, res) => {
+  const { id } = req.params;
+  const { content } = req.body;
+  const commentId = randomBytes(4).toString("hex");
+
+  const comments = COMMENTS[id] || [];
+
+  comments.push({ id: commentId, content });
+
+  COMMENTS[id] = comments;
+
+  res.status(201).json(COMMENTS[id]);
+});
+
+app.listen(4001, () => {
+  console.log("Listenin on 4001");
+});
