9a5721e5fd5a8b3f1d9fb9986357bfa1dd155c96
elgun <elgun.abasquliyev2@gmail.com>
2022-10-24 21:38:17 +0400

    Handling moderation

--- 2. A Mini-Microservices App/blog/event-bus/index.js
+++ 2. A Mini-Microservices App/blog/event-bus/index.js
@@ -2,11 +2,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const axios = require("axios");
 
-const SERVICES_URL = [
-  "http://localhost:4000/events",
-  "http://localhost:4001/events",
-  "http://localhost:4002/events",
-];
+const SERVICES = {
+  post: "http://localhost:4000/events",
+  comment: "http://localhost:4001/events",
+  query: "http://localhost:4002/events",
+  moderation: "http://localhost:4003/events",
+};
 
 const app = express();
 
@@ -15,7 +16,7 @@ app.use(bodyParser.json());
 app.post("/events", (req, res) => {
   const event = req.body;
 
-  SERVICES_URL.map((serviceUrl) => axios.post(serviceUrl, event));
+  Object.values(SERVICES).map((serviceUrl) => axios.post(serviceUrl, event));
 
   res.json({ status: "OK" });
 });
--- 2. A Mini-Microservices App/blog/moderation/index.js
+++ 2. A Mini-Microservices App/blog/moderation/index.js
@@ -1,12 +1,36 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const axios = require("axios");
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.post("/events", (req, res) => {
+app.post("/events", async (req, res) => {
   const { type, data } = req.body;
+
+  switch (type) {
+    case "CommentCreated":
+      {
+        const { id, content, postId } = data;
+
+        const status = content.includes("orange") ? "rejected" : "approved";
+
+        await axios.post("http://localhost:5000/events", {
+          type: "CommentModerated",
+          data: {
+            id,
+            content,
+            postId,
+            status,
+          },
+        });
+      }
+      break;
+
+    default:
+      break;
+  }
 
   res.json({ status: "OK" });
 });
