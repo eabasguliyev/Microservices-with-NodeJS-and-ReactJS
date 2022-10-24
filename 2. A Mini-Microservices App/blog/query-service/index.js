dc49b43670a4fc2da2c20e13985ac6ade29d6958
elgun <elgun.abasquliyev2@gmail.com>
2022-10-24 20:26:12 +0400

    Parse incoming events

--- 2. A Mini-Microservices App/blog/query-service/index.js
+++ 2. A Mini-Microservices App/blog/query-service/index.js
@@ -2,14 +2,63 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
+const POSTS = {};
+/**
+ *  QUICK EXAMPLE
+ * posts {
+ *  'key': {
+ *  id: "",
+ *  title: "",
+ *  comments: [{
+ *    id: '',
+ *    content: ""
+ *  },
+ *  {
+ *    id: '',
+ *    content: ""
+ *  }]
+ * }
+ * }
+ *
+ */
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get("/posts", (req, res) => {});
+app.get("/posts", (req, res) => {
+  res.json(POSTS);
+});
 
-app.post("/events", (req, res) => {});
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
+
+  switch (type) {
+    case "PostCreated": {
+      const { id, title } = data;
+
+      POSTS[id] = {
+        id,
+        title,
+        comments: [],
+      };
+      break;
+    }
+    case "CommentCreated": {
+      const { id, content, postId } = data;
+
+      const post = POSTS[postId];
+
+      post.comments.push({ id, content });
+      break;
+    }
+    default:
+      break;
+  }
+
+  console.log(POSTS);
+  res.json({});
+});
 
 app.listen(4002, () => {
   console.log("Listening on 4002");
