b65cea611b474c5335457c966a34dce3d51b3aa2
elgun <elgun.abasquliyev2@gmail.com>
2023-01-19 17:18:26 +0400

    NATS Streaming Server - An Event Bus Implementation

---
+++ 12. NATS Streaming Server - An Event Bus Implementation/backend/ticketing/nats-test/src/listener.ts
@@ -0,0 +1,52 @@
+import nats, { Message } from "node-nats-streaming";
+import { randomBytes } from "crypto";
+
+console.clear();
+
+const clientId = randomBytes(4).toString("hex");
+
+const stan = nats.connect("ticketing", clientId, {
+  url: "http://localhost:4222",
+});
+
+stan.on("connect", () => {
+  console.log("Listener connected to NATS");
+
+  stan.on("close", () => {
+    console.log("NATS connection closed");
+    process.exit();
+  });
+
+  const options = stan
+    .subscriptionOptions()
+    .setManualAckMode(true)
+    // Event Redelivery
+    .setDeliverAllAvailable()
+    .setDurableName("tickets-service "); // durable subscriptions
+
+  // .setMaxInFlight()
+  // .setAckWait()
+  // .setDeliverAllAvailable()
+  // .setDurableName();
+
+  const subscription = stan.subscribe(
+    "ticket:created",
+    "ticketServiceQueueGroup",
+    options
+  );
+
+  subscription.on("message", (msg: Message) => {
+    console.log("Message received");
+
+    const data = msg.getData();
+
+    if (typeof data === "string") {
+      console.log(`Received event $${msg.getSequence()}, with data: ${data}`);
+    }
+
+    msg.ack();
+  });
+});
+
+process.on("SIGINT", () => stan.close());
+process.on("SIGTERM", () => stan.close());
